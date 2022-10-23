3ffdc39923b52b8b964b73b7db5576e9cc13050e
J3R3D1CK232 <escobar.jeredick@gmail.com>
2022-10-23 15:55:47 -0600

    cambios

--- Proyecto_3/Properties/AssemblyInfo.cs
+++ Proyecto_3/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Proyecto_3")]
+[assembly: AssemblyTitle("Mi Buen Seguro - Aplicación")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
